ffcee4dae50fb80a7d1e040f29ffec72258f077d
ThreeToads <ivan.kokin.465@gmail.com>
2024-05-30 18:54:12 +0300

    fix(labs3): fix files comments

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,10 +1,10 @@
 // Copyright 2024 Kokin Ivan
 
-#include <gtest/gtest.h>  // NOLINT [build/c++11]
-#include <gmock/gmock.h>  // NOLINT [build/c++11]
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
 #include <cstdint>
-#include <thread>
-#include <chrono>
+#include <thread> // NOLINT [build/c++11]
+#include <chrono> // NOLINT [build/c++11]
 #include "TimedDoor.h"
 
 class MockTimerClient : public TimerClient {
